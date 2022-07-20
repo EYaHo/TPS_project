8104098022e73bbce80deb95c8b215de954d5b7d
NoMoreSpaghetti <h1103095@gmail.com>
2022-07-21 00:45:11 +0900

    Add Enemy Healthbar && Fix player shooting logic
    적의 체력바 추가
     - 플레이어 위치에 따라 체력바가 회전하여 적의 회전과 관계없이 항상 정면 상태의 체력바를 확인할 수 있다.
    
    PlayerShooter 코드를 추가하여 플레이어의 입력에 따른 총의 발사를 처리
    총알을 생성할 때 회전을 총의 회전과 같게 하여 총알의 방향 결정
    총알이 사정거리에 도달함을 확인하는 부분 수정

--- Assets/Scripts/InGame/EnemyHealth.cs
+++ Assets/Scripts/InGame/EnemyHealth.cs
@@ -1,8 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using Photon.Pun;
+using Cinemachine;
 
 public class EnemyHealth : LivingEntity
 {
-    
+    public Slider healthSlider;
+
+    [SerializeField]
+    private CinemachineVirtualCamera virtual_cam;
+    private EnemyMovement enemyMovement;
+
+    private void Awake() {
+        enemyMovement = GetComponent<EnemyMovement>();
+        virtual_cam = GameObject.FindWithTag("MainCamera").GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Update() {
+        healthSlider.transform.LookAt(healthSlider.transform.position + virtual_cam.transform.rotation * Vector3.back, virtual_cam.transform.rotation * Vector3.up);
+    }
+
+    protected override void OnEnable() {
+        base.OnEnable();
+
+        healthSlider.gameObject.SetActive(true);
+        healthSlider.maxValue = maxHealth;
+        healthSlider.value = health;
+    }
+
+    [PunRPC]
+    public override void RestoreHealth(float newHealth) {
+        base.RestoreHealth(newHealth);
+        healthSlider.value = health;
+    }
+
+    [PunRPC]
+    public override void OnDamage(float damage, Vector3 hitPoint, Vector3 hitDirection) {
+        base.OnDamage(damage, hitPoint, hitDirection);
+        healthSlider.value = health;
+    }
+
+    public override void Die() {
+        base.Die();
+
+        healthSlider.gameObject.SetActive(false);
+        enemyMovement.enabled = false;
+    }
 }
