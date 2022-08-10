228747bd88aa8916ecacaf078e4907faaebff2fa
NoMoreSpaghetti <h1103095@gmail.com>
2022-08-10 14:07:21 +0900

    Player animator IK setup

--- Assets/Scripts/InGame/Player/PlayerAnimationController.cs
+++ Assets/Scripts/InGame/Player/PlayerAnimationController.cs
@@ -23,6 +23,30 @@ public class PlayerAnimationController : AnimationController
 
     private string currentState_second;
 
+    public Transform leftHandMount;
+    public Transform rightHandMount;
+
+    public Vector3 leftHandPositionOffset = new Vector3(-1f, 0f, 0f);
+    public Vector3 leftHandRotationOffset = new Vector3(0f, -90f, 45f);
+    public Vector3 rightHandPositionOffset = new Vector3(-1f, 0f, 0f);
+    public Vector3 rightHandRotationOffset = new Vector3(0f, -90f, 45f);
+
+    private void OnAnimatorIK(int layerMask) {
+        Debug.Log("OnAnimatorIK");
+
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+        animator.SetIKPositionWeight(AvatarIKGoal.LeftHand, 1.0f);
+
+        animator.SetIKPosition(AvatarIKGoal.LeftHand, leftHandMount.position + leftHandPositionOffset);
+        animator.SetIKRotation(AvatarIKGoal.LeftHand, leftHandMount.rotation * Quaternion.Euler(leftHandRotationOffset));
+
+        animator.SetIKPositionWeight(AvatarIKGoal.RightHand, 1.0f);
+        animator.SetIKRotationWeight(AvatarIKGoal.RightHand, 1.0f);
+
+        animator.SetIKPosition(AvatarIKGoal.RightHand, rightHandMount.position + rightHandPositionOffset);
+        animator.SetIKRotation(AvatarIKGoal.RightHand, rightHandMount.rotation * Quaternion.Euler(rightHandRotationOffset));
+    }
+
     [PunRPC]
     public void ChangeAnimationState(string newState, int layer, float normalizedTime) { // layer = -1: base layer, layer = 1: upper layer
         if(layer == -1) {
--- Assets/Scripts/InGame/Player/RobotPlayer/RobotGunController.cs
+++ Assets/Scripts/InGame/Player/RobotPlayer/RobotGunController.cs
@@ -7,17 +7,16 @@ public class RobotGunController : GunController
     public float bulletLineLifeTime = 0.03f;
 
     public Transform spine;
-    public Transform arm_position_left;
-    public Transform arm_position_right;
+    public Transform armPositionLeft;
+    public Transform armPositionRight;
 
     private LineRenderer bulletLineRenderer;
     [SerializeField]
     private PlayerAnimationController animController;
     
-    private Vector3 aimPoint;
-    private Vector3 spine_rotation_offset = new Vector3(0, -40, -100);
-    private Vector3 arm_position_left_rotation_offset = new Vector3(0, 90, -45);
-    private Vector3 arm_position_right_rotation_offset = new Vector3(0, 90, -45);
+    private Vector3 spineRotationOffset = new Vector3(0, -40, -100);
+    private Vector3 armPositionLeftRotationOffset = new Vector3(0, 90, -45);
+    private Vector3 armPositionRightRotationOffset = new Vector3(0, 90, -45);
 
     private void Awake() {
         bulletLineRenderer = GetComponent<LineRenderer>();
@@ -25,7 +24,7 @@ public class RobotGunController : GunController
         bulletLineRenderer.positionCount = 2;
         bulletLineRenderer.enabled = false;
 
-        //animController = gameObject.transform.parent.GetComponent<PlayerAnimationController>();
+        animController = gameObject.transform.parent.GetComponent<PlayerAnimationController>();
     }
     private void Start() {
 
@@ -35,15 +34,15 @@ public class RobotGunController : GunController
         base.Update();
     }
     
-    
     private void LateUpdate() {
-        spine.LookAt(aimPoint);
-        spine.rotation = spine.rotation * Quaternion.Euler(spine_rotation_offset);
-        arm_position_left.LookAt(aimPoint);
-        arm_position_right.LookAt(aimPoint);
-        arm_position_left.rotation = arm_position_left.rotation * Quaternion.Euler(arm_position_left_rotation_offset);
-        arm_position_right.rotation = arm_position_right.rotation * Quaternion.Euler(arm_position_right_rotation_offset);
+        // spine.LookAt(aimPoint);
+        // spine.rotation = spine.rotation * Quaternion.Euler(spineRotationOffset);
 
+        armPositionRight.position += new Vector3(1f, 0f, 0f);
+        armPositionLeft.LookAt(aimPoint);
+        armPositionRight.LookAt(aimPoint);
+        armPositionLeft.rotation = armPositionLeft.rotation * Quaternion.Euler(armPositionLeftRotationOffset);
+        armPositionRight.rotation = armPositionRight.rotation * Quaternion.Euler(armPositionRightRotationOffset);
 
         //this.transform.LookAt(aimPoint);
         //transform.rotation = transform.rotation * Quaternion.Euler(offset);
@@ -51,23 +50,6 @@ public class RobotGunController : GunController
 
     protected override void OnEnable() {
         base.OnEnable();
-    }
-
-    public override Vector3 CalcAimVector() {
-        RaycastHit hitData;
-        Ray ray = Camera.main.ViewportPointToRay(new Vector3 (0.5f, 0.5f, 0));
-        //Vector3 aimPoint;
-
-        if(Physics.Raycast(ray, out hitData, attackRange, layerMask))
-        {
-            aimPoint = hitData.point;
-        }
-        else
-        {
-            aimPoint = ray.origin + ray.direction * attackRange;
-        }
-        
-        return (aimPoint - muzzle.position).normalized;
     }
 
     // 총알 생성
